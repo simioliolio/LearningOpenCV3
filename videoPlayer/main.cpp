f9e9b80afbaed067333104cc979e6585f983f78e
simioliolio <simon@oxygn.io>
2017-04-25 22:29:39 +0100

    Example 2-3

--- videoPlayer/main.cpp
+++ videoPlayer/main.cpp
@@ -7,9 +7,20 @@
 //
 
 #include <iostream>
+//#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    cv::namedWindow("Example3", cv::WINDOW_AUTOSIZE);
+    cv::VideoCapture cap;
+    cap.open( cv::String(argv[1]) );
+    
+    cv::Mat frame;
+    for (;;) {
+        cap >> frame;
+        if (frame.empty()) break;
+        cv::imshow( "Example 3", frame);
+        if (cv::waitKey(33) == 0) break;
+    }
 }
