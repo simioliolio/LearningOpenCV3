3eb2bf5d07547d2b8e74c9b16c1fae47cc08660a
simioliolio <simon@oxygn.io>
2017-04-25 21:47:53 +0100

    Initial Commit

---
+++ videoPlayer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  videoPlayer
+//
+//  Created by Simon Haycock on 25/04/2017.
+//  Copyright © 2017 oxygn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
